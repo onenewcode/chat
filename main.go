5a042e1ef2b9e9ff6565afa4d8d84b64fa7065da
29071 <lovestudy@qq.com>
2024-03-22 20:34:40 +0800

    添加静态资源

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"github.com/cloudwego/hertz/pkg/app/middlewares/server/recovery"
+	"github.com/cloudwego/hertz/pkg/app/server"
+)
+
+func main() {
+	h := server.New()
+	h.Use(recovery.Recovery()) // 可确保即使在处理请求过程中发生未预期的错误或异常，服务也能维持运行状态
+	h.Spin()
+}
