7f29559846b4a4ca924b16b13504f22b69721660
29071 <lovestudy@qq.com>
2024-04-02 14:04:39 +0800

    添加部分docker的内容，用于之后容器的构建

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# 继承的基础镜像
+FROM golang:1.22
+# 指定接下来的工作路径
+WORKDIR .
+# 定义一个windos系统里的环境变量
+#ENV VERSION=2.0.0	# optional
+
+# 将容器 3000 端口暴露出来， 允许外部连接这个端口
+EXPOSE 8082			#optional
+# 拷贝当前文件夹下的所有文件到，工作目录
+#COPY . .
+
+# 设置容器运行的命令
+CMD ["/cmd.sh"]
---
+++ cmd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e # 这一行设置了Shell脚本的退出状态行为。-e选项使得脚本在遇到任何命令返回非零退出状态时立即终止执行。
+if [ "$ENV" = 'DEV' ]; then
+ echo "Running Development Server" # 打印字符串
+# exec python "identidock.py" # 它将整个Shell脚本进程替换为运行Python解释器，并指定要执行的脚本文件为 "identidock.py"
+else
+ echo "Running Production Server"
+# exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+# --callable app --stats 0.0.0.0:9191
+fi
